a41a60c341ca5b1210db866da154e652e98d92bb
joão melo <j@melo.plus>
2024-10-10 17:31:25 -0300

    chore: cicd

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 export function main() {
-  import fs from "fs";
-  import path from "path";
+  // import fs from "fs";
+  // import path from "path";
   
-  const packagePath = path.resolve("package.json");
-  const readmePath = path.resolve("README.md");
+  // const packagePath = path.resolve("package.json");
+  // const readmePath = path.resolve("README.md");
   
-  const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
+  // const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
   
-  const readmeContent = `${packageJson.description}
+  // const readmeContent = `${packageJson.description}
   
-  \`\`\`bash
-  npm install ${packageJson.name}
-  \`\`\`
+  // \`\`\`bash
+  // npm install ${packageJson.name}
+  // \`\`\`
   
-  by ${packageJson.author}
-  `;
+  // by ${packageJson.author}
+  // `;
   
-  fs.writeFileSync(readmePath, readmeContent, "utf8");  
+  // fs.writeFileSync(readmePath, readmeContent, "utf8");  
 }
